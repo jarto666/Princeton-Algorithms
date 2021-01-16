5c7fd8a80eb94d4689ea7ca8d3619389f3f8adbd
ilya.livshits <jarto666@gmail.com>
2021-01-16 22:42:29 +0300

    Added link to the task

--- Week 1 - UnionFind - Assignment/Percolation.java
+++ Week 1 - UnionFind - Assignment/Percolation.java
@@ -4,6 +4,18 @@
  *  Last modified:     16/1/2021
  *****************************************************************************/
 
+/**
+ * Percolation. Given a composite systems comprised of randomly distributed
+ * insulating and metallic materials: what fraction of the materials
+ * need to be metallic so that the composite system is an electrical conductor?
+ * Given a porous landscape with water on the surface (or oil below),
+ * under what conditions will the water be able to drain through
+ * to the bottom (or the oil to gush through to the surface)?
+ * Scientists have defined an abstract process known as percolation to model such situations.
+ *
+ * TASK AND EXPLANATION: https://coursera.cs.princeton.edu/algs4/assignments/percolation/specification.php
+ */
+
 import edu.princeton.cs.algs4.WeightedQuickUnionUF;
 
 public class Percolation {
